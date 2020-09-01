81ff06b2e8ffbf45c2ab058d2b9f9602d6640b1b
Frederico Berchof <alberthdavid@MacBook-Air-de-Alberth.local>
2020-09-01 14:32:35 -0300

    System to exemplify the construction of a register developed in React

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Home" subtitle="React project!">
+        <div className="display-4">Welcome!</div>
+        <hr />
+        <p className="mb-0">System to exemplify the construction of a register developed in React!</p>
+    </Main>
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Made with <i className="fa fa-heart text-danger"></i> by
+            <strong> Frederico Berchof</strong>
+        </span>
+    </footer>
